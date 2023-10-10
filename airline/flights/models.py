ffd2ab190c6db103253586570514577142ae3cf1
ljw0201 <1290014124@qq.com>
2023-10-10 23:37:00 +0800

    Add airline

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AIRPORT(models.Model):
+    code=models.CharField(max_length=3)
+    city=models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.city} ({self.code})" 
+
 class  FLIGHT(models.Model):
-    origin=models.CharField(max_length=64)
-    destination=models.CharField(max_length=64)
+    origin=models.ForeignKey(AIRPORT,on_delete=models.CASCADE,related_name="depatures")
+    destination=models.ForeignKey(AIRPORT,on_delete=models.CASCADE,related_name="arrivals")
     duration=models.IntegerField()
 
     def __str__(self):
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import FLIGHT
 # Create your views here.
+def index(request):
+    return render(request,"flights/index.html",{
+        "flights":FLIGHT.objects.all()
+    })
